8f41c133879c962fc9e24fc25b7acf4c5490346e
Adham Kassab <adhams.ka7@gmail.com>
2024-11-17 15:01:09 +0200

    Update gitignore

--- src/Room/App.java
+++ src/Room/App.java
@@ -4,7 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
         RoomView view = new RoomView();
-        view.displayMenu();
+        view.showMenu();
 
     }
 }
