d32e3a5c172e079759bd681ff48f9e35da3e326b
Adham Kassab <adhams.ka7@gmail.com>
2024-11-17 14:47:30 +0200

    Implement Room View

--- src/Room/App.java
+++ src/Room/App.java
@@ -3,17 +3,8 @@ package Room;
 public class App {
     public static void main(String[] args) throws Exception {
         // System.out.println("Hello, World!");
-        RoomController controller = new RoomController();
-
-        Room room = controller.createRoom();
-
-        controller.readRoom(room);
-
-        room = controller.updateRoom(room);
-
-        controller.readRoom(room);
-
-        controller.deleteRoom(room);
+        RoomView view = new RoomView();
+        view.displayMenu();
 
     }
 }
