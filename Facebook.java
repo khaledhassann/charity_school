a985a3dba4284ebc0f134eed82c4c638a5e8fa23
Omar Sobhy <81743860+Omarr14@users.noreply.github.com>
2025-01-17 16:22:19 +0200

    Update Facebook.java

---
+++ Facebook.java
@@ -0,0 +1,37 @@
+package com.rungroup.web.models;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name="Facebook")
+public class Facebook extends Advertisement {
+    private String PlatformAlgo;
+    public Facebook( String content,String DecoratedAd,  String status, LocalDateTime launchDate, Long eventId) {
+        super(content, DecoratedAd,status, launchDate, eventId);
+    }
+    @Override
+    public String showAdString(){
+        return "Facebook";
+    }
+    public String FacebookPost(){
+        return "";
+    }
+    
+
+    
+}
